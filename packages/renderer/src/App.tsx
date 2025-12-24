30fcc077759ef0298735df245e63be446db3c4e1
hbouhadji <bouhadji.hakim@gmail.com>
2025-12-24 05:31:08 +0100

    chore: add unocss + presetWind4

--- packages/renderer/src/App.tsx
+++ packages/renderer/src/App.tsx
@@ -1,23 +1,11 @@
-import { createSignal } from 'solid-js'
+import 'virtual:uno.css'
 
 function App() {
-  const [count, setCount] = createSignal(0)
-
   return (
-    <>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
-    </>
+    <div class="h-screen bg-black flex">
+      <div class="bg-red-500 flex-1">Item 1</div>
+      <div class="bg-blue-500 flex-1">Item 2</div>
+    </div>
   )
 }
 
